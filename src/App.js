93d94b3b9e431eddb5eed7f3c9603e3ef383359f
SonalKhapre <137900422+SonalKhapre@users.noreply.github.com>
2023-09-24 15:29:25 +0530

    exact path added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   return (
     <div>
     <CartProvider>
-   <BrowserRouter basename='frontend-project-restaurant'>
+   <BrowserRouter basename='/frontend-project-restaurant'>
     <Routes>
-      <Route path="/" element={ <Home /> } />
+      <Route exact path="/" element={ <Home/>} />
       <Route path="/about" element={ <About /> }/>
       <Route path="/contact" element={ <Contact /> }/>
       <Route path="/menu" element={ <Menu /> }/>
